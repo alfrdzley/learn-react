39f62cfa2ce1920be29549f9d8ab8ff2fdb89a90
SYERA AL-FARIDZI <2311102441242@umkt.ac.id>
2024-07-04 08:50:01 +0800

    chore: Refactor App component to display a todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Gallery from './Gallery.jsx';
-import Profile from './Profile.jsx';
-
-export default function App() {
-  return(
-    <div>
-    <Profile />
-    <Gallery />
-    </div>
-  );
+export default function TodoList() {
+  return (
+    <>
+    <h1>
+      SYERA AL-FARIDZI
+    </h1>
+    <img src="https://avatars.githubusercontent.com/u/9143296?v=4" alt="Syera Al-Faridzi" width="200" />
+    <ul>
+      <li>Belajar React</li>
+      <li>Belajar Vue</li>
+      <li>Belajar Angular</li>
+    </ul>
+    </>
+  )
 }
